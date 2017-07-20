ff9b5b8bdab6b5f0c45c118ec467e73d77dda971
Tupinier <julien_tupinier@hotmail.fr>
2017-07-20 10:25:28 +0200

    modification btn et ajout option one page grid

--- scripts/following.js
+++ scripts/following.js
@@ -1,6 +1,7 @@
-        //permet de garder le menu en haut après l'avoir dépassé après un certain temps
-        //pour coller un élément de la page au passage remplacer outerHeight par offset().top;
-        //nécessite que la block concerné possède la classe stickToTop
+//permet de garder le menu en haut après l'avoir dépassé après un certain temps
+//en lui ajoutant la classe fixed
+//pour coller un élément de la page au passage remplacer outerHeight par offset().top;
+//nécessite que le block concerné possède la classe stickToTop
 
 if (window.matchMedia("(min-width: 500px)").matches){
     
