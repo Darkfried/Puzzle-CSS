fb87a5f919447a78d1355ec50730954ebe258d37
Tupinier <julien_tupinier@hotmail.fr>
2017-07-20 07:49:35 +0200

    Initial concept commit

---
+++ scripts/following.js
@@ -0,0 +1,26 @@
+        //permet de garder le menu en haut après l'avoir dépassé après un certain temps
+        //pour coller un élément de la page au passage remplacer outerHeight par offset().top;
+        //nécessite que la block concerné possède la classe stickToTop
+
+if (window.matchMedia("(min-width: 500px)").matches){
+    
+        var stickOffset = $('.stickToTop').outerHeight();
+        var time = 400;
+
+        $(window).scroll(function () {
+
+            setTimeout(function () {
+
+                var fixed = $('.stickToTop'),
+                    scroll = $(window).scrollTop()
+
+                if (scroll > stickOffset) {
+                    fixed.addClass('fixed');
+                    
+                } else {
+                    fixed.removeClass('fixed');
+                }
+
+            }, time);
+        });
+}
